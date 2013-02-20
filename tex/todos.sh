5ce1f7b658d0adceeff917d193ff559aea92f30d
leonardofl <leofl@ime.usp.br>
2013-02-19 22:34:40 -0300

    versão da quali entregue para a banca

--- tex/todos.sh
+++ tex/todos.sh
@@ -1,2 +1,4 @@
 # encontra os ToDos
-ls *.tex | xargs grep '\Leo{' --color -n
+ls *.tex | xargs grep -i '\Leo{' --color -n
+ls *.tex | xargs grep -i '\Fabio{' --color -n
+ls *.tex | xargs grep -i '\Gerosa{' --color -n
