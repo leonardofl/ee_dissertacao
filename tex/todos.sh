4165f46e6854e4a177751ffbf73a75730c155969
leonardofl <leonardofl87@gmail.com>
2013-12-27 12:50:30 -0200

    mudando burocracias de quali para dissertação

--- tex/todos.sh
+++ tex/todos.sh
@@ -1,4 +1,5 @@
 # encontra os ToDos
-ls *.tex | xargs grep -i '\Leo{' --color -n
-ls *.tex | xargs grep -i '\Fabio{' --color -n
-ls *.tex | xargs grep -i '\Gerosa{' --color -n
+ls *.tex | xargs grep -i '\\Leo{' --color -n
+ls *.tex | xargs grep -i '\\Fabio{' --color -n
+ls *.tex | xargs grep -i '\\Gerosa{' --color -n
+ls *.tex | xargs grep -i '\\todo{' --color -n
