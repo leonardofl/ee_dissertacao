68a90939471bff35603e5e4b7c03437a530dff94
leonardofl <leonardofl87@gmail.com>
2014-03-30 18:00:01 -0300

    movendo arquivos

--- merged-with-user-guide/merge.sh
+++ merged-with-user-guide/merge.sh
@@ -6,10 +6,10 @@
 # A paginação dos arquivos de entrada já deve estar organizada.
 # Obs: comando para alterar paginação: \setcounter{page}{87}
 
-pdftk ../dissertacao.pdf cat 1-65 output parte1.pdf dont_ask 
-pdftk ../dissertacao.pdf cat 66-end output bibliografia.pdf dont_ask
+pdftk ../tex/dissertacao.pdf cat 1-65 output parte1.pdf dont_ask 
+pdftk ../tex/dissertacao.pdf cat 66-end output bibliografia.pdf dont_ask
 pdftk parte1.pdf ee_user_guide.pdf bibliografia.pdf cat output final-noinfo.pdf dont_ask 
-pdftk ../dissertacao.pdf dump_data > metadata.txt
+pdftk ../tex/dissertacao.pdf dump_data > metadata.txt
 sed -i 's/BookmarkPageNumber: 66/BookmarkPageNumber: 103/' metadata.txt
 pdftk final-noinfo.pdf update_info metadata.txt output dissertacao-leo.pdf dont_ask
 rm parte1.pdf bibliografia.pdf final-noinfo.pdf metadata.txt
